35866abc8a5f1a8e57be22b81db634733a337733
Harsh <harshj7.net@gmail.com>
2022-12-28 17:49:57 +0530

    add 13 string js till 77 line

--- js/13-string-manipulation.js
+++ js/13-string-manipulation.js
@@ -23,4 +23,55 @@ let str = "Harsh";
 document.write(str[3]); //s
 */
 
+/* concatinate 2 strings or add 2 strings
 let str = "Harsh";
+let str2 = "Kumar";
+document.write(str + " " + str2);
+*/
+
+/*compare 2 strings*/
+/*let str = "Harsh";
+if (str == "Harsh") {
+    document.write("Equal");
+}
+else {
+    document.write("Not Equal");
+}
+*/
+// javascript is case sensitive
+
+// functions to manipulate string in js
+// to add 2 string we use + opreator or use concat function
+/*let str = "Harsh";
+let str2 = "Kumar";
+let str3 = str.concat(str2);
+document.write(str3); //HarshKumar
+document.write(" ", str3); //HarshKumar
+str3 = str.concat(" ", str2);
+document.write(str3); //Harsh Kumar
+*/
+
+// string se ek substring nikalna: string se ek chota tukra nikalna
+/*let str = "This is Javascript tutorial";
+let str2 = str.substr(8, 19); //8:first character of substring is at, 19: length of substring i.e 8 va character se start karo aur 19 we character tK jao
+document.write(str2); //avascript tutorial
+
+
+str2 = str.substring(8, 18); //8va char se start karo aur 18ve character tak jao
+document.write(str2); //Javascript
+*/
+
+//kisi v string ka position janana chahte hain
+
+let str = "I love js becuse it is Good";
+let pos = str.indexOf("Good"); //23 if the string inside (" ") will not be in str variable then it will rreturn -1
+document.write(pos);
+
+
+let nam = "Hey everyone welcome back to Hey.";
+let nam2 = nam.indexOf("Hey");
+document.write(nam2); //0 as first Hey is at 0. also it will fetch "is in This".
+
+//if we want to know position of other Hey rather than first Hey then we have to pass another parameter
+nam2 = nam.indexOf("Hey", 10); //hey after 10th character. agar 1st string find karna hai then no need of parameter
+document.write(nam2);  //29
