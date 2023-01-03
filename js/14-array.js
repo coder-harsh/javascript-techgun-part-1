c93ef41fa187260890c814045d020ea0800f8645
Harsh <harshj7.net@gmail.com>
2023-01-04 01:12:00 +0530

    add 14 array js file code till 30

---
+++ js/14-array.js
@@ -0,0 +1,30 @@
+/*
+let i;
+for (i = 0; i < 3; i++) {
+    document.write(book[i] + " "); //Math pps GK
+    i = i++;
+}
+
+
+//array: a variable is used to store 1 or more than 1 data. type of data may be diff. in array
+//agr data string h to curli bracket mein store karo. agar no h to aise hi , se seprate karo.
+//array index 0 se start hota h.
+// let book = ["Math", "pps", "GK"];
+let book = new Array("Math", "pps", "GK"); //both will declare book array
+
+
+let num = [1, 22, 34, "Harsh"];
+document.write(num[1]); //22
+document.write(num[3]); //Harsh
+
+
+//how to see full array
+let num = [1, 2, 3];
+document.write(num); //1,2,3
+*/
+
+
+//update an array
+let num = [1, 2, 3];
+num[1] = 33;
+document.write(num); //2 ke jagah 33 aa jayega
