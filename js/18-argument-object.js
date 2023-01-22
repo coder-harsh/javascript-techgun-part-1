67f96be74fb075fafdd339b35165e667df71069f
Harsh <harshj7.net@gmail.com>
2023-01-22 23:24:10 +0530

    add 18 argument-object js till line 4

---
+++ js/18-argument-object.js
@@ -0,0 +1,4 @@
+/*
+pichhle video mein hum function mein hum 2 argument send karte the aur function 2 hi parameter receive karta tha. aur add function 2 no hi add kar sakta tha.
+but hum ab cahte hain ki agar hum 3 argument bhi send kare to 3 no ko bhi function add kar de, uske liye hum the arguments object ka use karte hain.
+*/
