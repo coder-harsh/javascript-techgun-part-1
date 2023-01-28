f724cb8fcd54c39d5fda326c08f8d78bc24d57ad
Harsh <harshj7.net@gmail.com>
2023-01-28 10:35:25 +0530

    add 23-object in js all till 47 no line

---
+++ js/23-object-in-js.js
@@ -0,0 +1,47 @@
+/*
+//object mtlb kuch chij. ex: football, car, human body, cycle etc.
+
+//object hone k liye 2 chij necessary h.
+1. properties
+2. uspe kuch action/function hota h, ya usse kuch action/function hota h
+
+//programming m action ko function y method(js me) bolte hn
+
+ex: human
+human property
+name
+height
+mobile no
+address etc.
+
+object action/function
+eat
+walk
+talk
+run
+study
+
+ex: car
+car property
+brand
+model
+variant
+
+object action/function
+drive
+parking
+servicing
+
+
+
+objects in programming
+student, teacher, course in learning mmanagement system
+
+account, account holder, cashier, bank manager etc. in banking mnagement system
+
+book, department etc. in library management system
+
+noun ek tarah se programming m object h
+
+this is all object oriented programming
+*/
