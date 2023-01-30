d8397972d84c955db2ae360e58507409b2d6de61
Harsh <harshj7.net@gmail.com>
2023-01-30 22:01:09 +0530

    add 25 methods in object till 21 line

--- js/25-methods-in-object.js
+++ js/25-methods-in-object.js
@@ -1,1 +1,21 @@
-//method action or properties
+//ye object ab kuch action karega. action k liye kuch function bannyenege. function ko yaha javascript mein method bol rahe hain.
+//wiasa function jo kisi object k sath assosiated ho. kisi object k function ko hum method bolenege
+
+//let create a object person
+
+let person = {
+    firstName: 'Harsh',
+    lastName: 'Kumar'
+};
+
+person.age = 25;
+
+//hum 4 tarike se method bana sakte hain
+
+person.sayHello = function () {//sayHello: method name, anonymous function
+    console.log("Hello");
+}
+
+person.sayHello();//method calling
+
+//2nd tarika for method
