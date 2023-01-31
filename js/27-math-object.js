55509c5f4f43ca947c66808f03cae1ae3ddae886
Harsh <harshj7.net@gmail.com>
2023-01-31 18:40:53 +0530

    add 27 math object all till 44 line

---
+++ js/27-math-object.js
@@ -0,0 +1,46 @@
+//kuch object js m already bana banaya h
+//math object math k sare problem solve karne m help karta h
+//math object k pass kuch kuch function/method hog aur kuch 2 property hoga
+/*
+//math properties
+document.write(Math.PI); //3.141592653589793
+document.write(Math.E); //2.718281828459045
+
+//pi and E is the property
+
+
+//math name k object me kaun 2 function uske andr h
+//value round karna
+var x = Math.round(4.7); //5
+var x = Math.round(4.3); //5
+console.log(x);
+
+
+var x = Math.ceil(4.2); //5 no se thoda v jyda hu value to next no dikhao
+var x = Math.floor(4.7); //4  no se thoda v jydda ho to bottom wala no dikhao
+console.log(x);//4
+
+//round ceil aur floor similar work hi karta hai
+
+//koi v no ho point hata k batao
+
+var x = Math.trunc(4.7); //4 .decimal remove kra do
+var x = Math.trunc(4.5000737487); //4
+console.log(x);
+
+
+document.write(Math.pow(3, 2)); //9 | 3base power2
+document.write(Math.sin(0)); //0
+document.write(Math.sqrt(4)); //2
+*/
+
+var x = Math.min(4, 6, 7, 7889, 9); //4
+var x = Math.max(4, 6, 7, 7889, 9); //7889
+var x = Math.random(); //0.8680477759890326
+var x = Math.random(); //0.8483635256710564 //return random no bw 0 and 1
+document.write(x);
+
+
+//math ka koi v problemn aaye to math object function and property dekhna
+
+
