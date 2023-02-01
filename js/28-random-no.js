d2a12adb6fe5407d9879bf1088e6e43c74ad2552
Harsh <harshj7.net@gmail.com>
2023-02-01 23:54:56 +0530

    add 28 random no till 20 line

---
+++ js/28-random-no.js
@@ -0,0 +1,21 @@
+//javascript mein random no kaise genrate kiya jata hain
+//ludo game mein no genarate karna h
+//captcha no
+//isme hume math object kam aata h
+/*
+var x = Math.random(); //page refresh p no change hoga | 0-1 k bich no generate karega hmesha i.e 0 aur 1 choor k
+document.write(x);
+
+
+//hum esse useful bana ke 0-9 k bich no generate karna chahte hn
+//abhi only 0 se uppar ka no generate karega
+var x = Math.floor(Math.random() * 10); //0 se 9 k bich no genrate karega
+var x = Math.floor(Math.random() * 11); //0 se 10 k bich no genrate karega | jitna no ka limit chahiye usse ek jyda se multiply karenge
+var x = Math.floor(Math.random() * 101); //0 se 100 tk no generate karna
+var x = Math.floor(Math.random() * 10) + 1; //0 se 10 tk no generate karna
+document.write(x);
+*/
+
+//hum ab 2 no k bich ka no generate krna chahte only.
+console.log("Hi");
+
