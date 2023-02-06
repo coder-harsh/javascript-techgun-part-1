4dba820e0d10c02fd790241b3807026d670729fd
Harsh <harshj7.net@gmail.com>
2023-02-06 22:45:15 +0530

    add 30 new keyword till 13 line

---
+++ js/30-new-keyword.js
@@ -0,0 +1,13 @@
+//abhi tak hum aise object banate the
+/*
+
+let person = { //object name
+    //yah property value ya method likheneg
+    height: 20,
+    width: 30,
+    Name_person: "Harsh"
+}
+//access karmne k liye
+console.log(person.Name_person); //Harsh
+*/
+// ab dusre tarike se
