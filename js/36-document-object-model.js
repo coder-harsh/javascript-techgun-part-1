b8d960392cf6d723340bd5677e99f5166258f524
Harsh <harshj7.net@gmail.com>
2023-02-09 22:55:20 +0530

    add 36 dom till line 6

---
+++ js/36-document-object-model.js
@@ -0,0 +1,6 @@
+//dom:document object model Bom:browser object model
+
+//imp topic
+// document object model(dom) is an application programming interface(api) for manipulating HTML and xml documents. ex: add, remove, and modify parts of document/html
+//dom help to add delete modify html.
+//js se html m change late hn api k through. us api ka name h dom
