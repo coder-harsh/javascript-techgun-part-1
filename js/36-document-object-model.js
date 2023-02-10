745c11fc718a4bf532eb62c2e75cb0e586abb7f7
Harsh <harshj7.net@gmail.com>
2023-02-10 18:12:40 +0530

    add all 36 dom till 29 line

--- js/36-document-object-model.js
+++ js/36-document-object-model.js
@@ -4,3 +4,26 @@
 // document object model(dom) is an application programming interface(api) for manipulating HTML and xml documents. ex: add, remove, and modify parts of document/html
 //dom help to add delete modify html.
 //js se html m change late hn api k through. us api ka name h dom
+
+///when the page load in browser. then browser creates a  tree like structure called dom tree
+//in dom tree the document is root node or object
+
+/*
+js can change all the html elements
+js can change all the html attributes
+js can change all the css styles
+js can remove existing html elements and attributes
+js can add new html elements and attributes
+js cn react to all existing html events in the page
+js can create new html events in page
+
+
+in dom  all html elements are defined as objects. so it will have both property and method.
+har object p hum property ya method k through kam kar sakte hn.
+//har element object p kam krne k liye kuchh property ya method hn
+
+the document object represents your web page.
+kuch v karne k liye document object k use krna padega. document.method/function_name/properties
+
+if you want to access any element in an html page, you always start with accessing the document object.
+*/
